1b18865b41361dfc1a9d4d9fdd4ae9953e884245
Raj <raj.malviya456@gmail.com>
2021-04-08 23:36:45 +0530

    Associate Courses to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@yopmail.com', password: 'password', password_confirmation: 'password')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
